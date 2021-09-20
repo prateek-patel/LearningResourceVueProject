a895e55debcb3800ff089f03f93fe997af9ac23f
Prateek Patel <cwr.prateek.patel@asurion.com>
2021-09-20 15:35:07 +0530

    error dialog teleported to body tag of html page

--- src/components/UI/BaseDialog.vue
+++ src/components/UI/BaseDialog.vue
@@ -1,4 +1,5 @@
 <template>
+<teleport to="body">
   <div @click="$emit('close')"></div>
   <dialog open>
     <header>
@@ -16,6 +17,7 @@
       </slot>
     </menu>
   </dialog>
+  </teleport>
 </template>
 
 <script>
